dfb8e5cb4cb4f4097442901c0f19052edd3c63df
Tony Fast <tony.fast@gmail.com>
2020-07-19 17:15:49 -0400

    Create __init__.py

---
+++ xlbs/__init__.py
@@ -0,0 +1,12 @@
+"""A performance about programming
+
+* computational skateparks
+* (demo)nstration
+* hyperactive computing
+* (demo)lition
+* dead pixels
+"""
+__version__ = "12.09.68"
+
+with __import__('pidgy').pidgyLoader():
+  from . import readme
